dae425572678852370d2e1ce38205c4b81759467
nagalauttimur <153740298+nagalauttimur@users.noreply.github.com>
2023-12-13 22:31:55 +0800

    Add files via upload

---
+++ Xtramiles - Christian/WeatherApp/Bootstrapper.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
+using WeatherApp.Service.Country;
+using WeatherApp.Service.Weather;
+
+namespace WeatherApp
+{
+    public static class Bootstrapper
+    {
+        #region Properties
+
+        public static UnityContainer Container
+        {
+            get { return (UnityContainer)BuildUnityContainer(); }
+        }
+
+        #endregion Properties
+
+        #region (private) Methods
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<ICountryService, CountryService>();
+            container.RegisterType<IWeatherService, WeatherService>();
+
+            return container;
+        }
+
+        private static void RegisterTypes(Dictionary<Type, Type> containerTypes, UnityContainer container)
+        {
+            foreach (var item in containerTypes)
+            {
+                container.RegisterType(item.Key, item.Value);
+            }
+        }
+
+        #endregion (private) Methods
+
+        #region (public) Methods
+
+        public static IUnityContainer Initialise()
+        {
+            var container = BuildUnityContainer();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+
+            //var unityServiceLocator = new UnityServiceLocator(container);
+            //ServiceLocator.SetLocatorProvider(() => unityServiceLocator);
+
+            return container;
+        }
+
+        #endregion (public) Methods
+    }
+}
